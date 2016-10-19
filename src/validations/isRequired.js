1dac17d97b03eeea6962b50fa34c3484cc85d6df
Stan Bondi <stan@fixate.it>
2016-10-19 13:49:23 +0200

    Make more flexible

--- src/validations/isRequired.js
+++ src/validations/isRequired.js
@@ -1,7 +1,7 @@
-export default function isRequired(name) {
+export default function isRequired(extra) {
   return (value) => {
     if (value === undefined || value === null || value === '') {
-      return { type: 'required', reason: 'empty', value, name };
+      return Object.assign({ type: 'required', reason: 'empty', value }, extra);
     }
 
     return false;
