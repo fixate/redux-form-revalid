df066da03176c86d28285bc9604d548c74c29552
Larry Botha <larry@fixate.it>
2018-11-16 15:29:39 +0200

    add check for invalid length

--- src/validations/isRequired.js
+++ src/validations/isRequired.js
@@ -1,7 +1,12 @@
 export default function isRequired(extra) {
-  return (value) => {
-    if (value === undefined || value === null || value === '') {
-      return Object.assign({ type: 'required', reason: 'empty', value }, extra);
+  return value => {
+    if (
+      value === undefined ||
+      value === null ||
+      value === '' ||
+      !value.length
+    ) {
+      return Object.assign({type: 'required', reason: 'empty', value}, extra);
     }
 
     return false;
